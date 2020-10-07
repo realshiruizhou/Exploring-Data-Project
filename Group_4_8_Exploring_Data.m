e9395b37048e1bc347187a322c1c97fe4314ba58
Shirui Zhou <45808099+realshiruizhou@users.noreply.github.com>
2020-10-07 17:42:15 -0400

    Update Group_4_8_Exploring_Data.m

--- Group_4_8_Exploring_Data.m
+++ Group_4_8_Exploring_Data.m
@@ -1,3 +1,9 @@
 % Team 4-8 Coronavirus Data Report
 % Authors: Claire Kucera, Ian Tracy, Matthew Fortuna, Gabe Montemayor,
 % Niloy Neogi, Shirui Zhou
+
+clear; %clear workspace
+clc; %clear command window
+
+%% Read the data into a table
+data = readtable('COVID_Data_2020_10_07.xlsx');
